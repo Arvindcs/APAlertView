d58797dad0045ccf0061a0d805b36a0dd7c0a63d
Arvind <arvindpatelcs@gmail.com>
2022-03-31 14:48:41 +0530

    added actionsheet screenshot

--- APAlertView/ContentView.swift
+++ APAlertView/ContentView.swift
@@ -20,10 +20,6 @@ struct ContentView: View {
                     debugPrint("OK Button Pressed")
                 }
             }
-            .padding(.init(top: 10, leading: 10, bottom: 10, trailing: 10))
-            .foregroundColor(.white)
-            .background(Color.black)
-            .cornerRadius(6)
             
             Button("Show Alert with Two Button") {
                 alertView.showAlertWithTwoButton(title: "Alert", message: "This is APAlertView", firstButtonTitle: "Submit", secondButtonTitle: "Cancel") {
@@ -32,10 +28,7 @@ struct ContentView: View {
                     debugPrint("Cancel Button Pressed")
                 }
             }
-            .padding(.init(top: 10, leading: 10, bottom:10, trailing: 10))
-            .foregroundColor(.white)
-            .background(Color.black)
-            .cornerRadius(6)
+           
             
             Button("Show ActionSheet") {
               alertView.showActionSheet(title: "This is an ActionSheet", firstButtonTitle: "Add User", secondButtonTitle: "Edit User") {
@@ -44,11 +37,7 @@ struct ContentView: View {
                     debugPrint("Edit User Button Pressed")
                 }
             }
-            .padding(.init(top: 10, leading: 10, bottom:10, trailing: 10))
-            .foregroundColor(.white)
-            .background(Color.black)
-            .cornerRadius(6)
-                        
+           
         }
         .initializeAlert(alertView)
         .initializeAlertActionSheet(alertView.actionSheet)
